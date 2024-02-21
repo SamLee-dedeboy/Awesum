9d913c044a6448d5517771f9249df09d05d57b4e
Aryaman333 <abahukhandi@ucdavis.edu>
2024-02-21 15:34:51 -0800

    Added Linguistic_Metrics.py for all linguistic metrics and naturalness analysis. Stylistic_Metrics_test.ipynb for all experiments conducted and examples on how to use the classes

--- preprocess/experiments/metrics/Linguistic_Metrics.py
+++ preprocess/experiments/metrics/Linguistic_Metrics.py
@@ -18,12 +18,111 @@ nltk.download('stanford-corenlp')
 nltk.download('stanford-corenlp-4.2.0')
 stanza.download('en')       
 
-class Taggers:
+class naturalness:
+    def __init__(self,df): #Pass empty string to load all processors or choose from the above options and pass in 1 comma seperated string
+        self.nlp = spacy.load("en_core_web_sm")
+        self.weights = {
+            'Average Dependency tree heights': 0.2826426317853948,
+            'average_sentence_lengths': 0.2522139996530825,
+            'avg_left_subtree_height': 0.23287886122532872,
+            'avg_right_subtree_height': 0.2458490930520618,
+        }
+        self.range_avg_dep_tree_ht = (min(df['Writer Average Dependency tree heights'].min(),df['LLM Average Dependency tree heights'].min()),max(df['Writer Average Dependency tree heights'].max(),df['LLM Average Dependency tree heights'].max()))
+        self.range_avg_sentence_length = (min(df['writer_average_sentence_lengths'].min(),df['LLM_average_sentence_lengths'].min()),max(df['writer_average_sentence_lengths'].max(),df['LLM_average_sentence_lengths'].max()))
+        self.range_avg_left_subtree_ht = (min(df['writer_avg_left_subtree_height'].min(),df['LLM_avg_left_subtree_height'].min()),max(df['writer_avg_left_subtree_height'].max(),df['LLM_avg_left_subtree_height'].max()))
+        self.range_avg_right_subtree_ht = (min(df['writer_avg_right_subtree_height'].min(),df['LLM_avg_right_subtree_height'].min()),max(df['writer_avg_right_subtree_height'].max(),df['LLM_avg_right_subtree_height'].max()))
+        self.ranges = np.array([self.range_avg_dep_tree_ht,self.range_avg_sentence_length,self.range_avg_left_subtree_ht,self.range_avg_right_subtree_ht])
+        # self.writer_avg_sentence_length_range = (self.dataset['writer_average_sentence_lengths'].min(),self.dataset['writer_average_sentence_lengths'].max())
+        # self.llm_avg_sentence_length_range = (self.dataset['LLM_average_sentence_lengths'].min(),self.dataset['LLM_average_sentence_lengths'].max())
+        # self.Writer_avg_dep_tree_heights_range =(self.dataset['Writer Average Dependency tree heights'].min(),self.dataset['Writer Average Dependency tree heights'].max())
+        # self.LLM_avg_dep_tree_heights_range =(self.dataset['LLM Average Dependency tree heights'].min(),self.dataset['LLM Average Dependency tree heights'].max())
+        # self.writer_avg_left_subtree_height_range = (self.dataset['writer_avg_left_subtree_height'].min(),self.dataset['writer_avg_left_subtree_height'].max()) 
+        # self.LLM_avg_left_subtree_height_range = (self.dataset['LLM_avg_left_subtree_height'].min(),self.dataset['LLM_avg_left_subtree_height'].max()) 
+        # self.writer_avg_righht_subtree_height_range = (self.dataset['writer_avg_right_subtree_height'].min(),self.dataset['writer_avg_right_subtree_height'].max()) 
+        # self.LLM_avg_right_subtree_height_range = (self.dataset['LLM_avg_right_subtree_height'].min(),self.dataset['LLM_avg_right_subtree_height'].max()) 
+
+    def naturalness_helper(self,features, weights):
+        inverses = 1.0-features
+        naturalness_score = np.mean(np.dot(inverses, weights))
+        return naturalness_score
+
+    def min_max_scaling(self,feature,range):
+        scaled_feature = (feature-range[0])/(range[1]-range[0])
+        return scaled_feature
+
+    def calculate_naturalness(self,text):
+        avg_sentence_length = self.avg_sentence_length(text)
+        avg_dep_tree_ht = self.calculate_average_tree_height([text])
+        avg_left_subtree_height, avg_right_subtree_height = self.calculate_subtree_features(text)
+        features = np.array([avg_dep_tree_ht,avg_sentence_length,avg_left_subtree_height,avg_right_subtree_height])
+        zipped_features = list(zip(features,self.ranges))
+        scaled_features = []
+        for feature,tup in zipped_features:
+            scaled_features.append(self.min_max_scaling(feature,tup))
+        naturalness = self.naturalness_helper(np.array(scaled_features),np.array(list(self.weights.values())))
+        return naturalness
+    
+    def calculate_subtree_features(self,text):
+        doc = self.nlp(text)
+
+        def subtree_features(token):
+            lefts = list(token.lefts)
+            rights = list(token.rights)
+
+            # Count the number of left and right subtrees
+            num_left_subtrees = len(lefts)
+            num_right_subtrees = len(rights)
+
+            # Calculate the average left and right subtree height
+            avg_left_subtree_height = sum(t._.depth for t in lefts) / num_left_subtrees if num_left_subtrees > 0 else 0
+            avg_right_subtree_height = sum(t._.depth for t in rights) / num_right_subtrees if num_right_subtrees > 0 else 0
+            return num_left_subtrees, num_right_subtrees, avg_left_subtree_height, avg_right_subtree_height
+        
+        spacy.tokens.Token.set_extension('depth', getter=lambda token: len(list(token.ancestors)), force=True)
+        total_left_subtrees = 0
+        total_right_subtrees = 0
+        total_left_subtree_height = 0
+        total_right_subtree_height = 0
+        for sent in doc.sents:
+            for token in sent:
+                num_left_subtrees, num_right_subtrees, avg_left_subtree_height, avg_right_subtree_height = subtree_features(token)
+                total_left_subtrees += num_left_subtrees
+                total_right_subtrees += num_right_subtrees
+                total_left_subtree_height += avg_left_subtree_height * num_left_subtrees
+                total_right_subtree_height += avg_right_subtree_height * num_right_subtrees
+        avg_left_subtree_height_text = total_left_subtree_height / total_left_subtrees if total_left_subtrees > 0 else 0
+        avg_right_subtree_height_text = total_right_subtree_height / total_right_subtrees if total_right_subtrees > 0 else 0
+
+        return avg_left_subtree_height_text,avg_right_subtree_height_text
+    
+    def calculate_average_tree_height(self,text_list):
+        total_tree_height = 0
+        num_trees = 0
+        for text in text_list:
+            doc = self.nlp(text)
+            stack = [(sent.root, 1) for sent in doc.sents]
+
+            while stack:
+                node, depth = stack.pop()
+                total_tree_height += depth
+                num_trees += 1
+                stack.extend((child, depth + 1) for child in node.children)
+        avg_tree_height = total_tree_height / max(1, num_trees)
+        return avg_tree_height
+    
+    def avg_sentence_length(self,text):
+        total_tokens = 0
+        sentences= nltk.sent_tokenize(text)
+        total_tokens = sum(len(sentence.split()) for sentence in sentences)
+        average_length= total_tokens / len(sentences)
+        return average_length
+
+class LinguisticFeatures:
 
     # Processor Options: tokenize, mwt (multi-word-tokens processor), pos (universal pos, treebank-specific POS (XPOS),  universal morphological features (UFeats)), 
     # lemma, depparse(dependency parsing), ner, sentiment(-ve,neutral,+ve), constituency
     
-    def __init__(self,processors = None, use_gpu=False,): #Pass empty string to load all processors or choose from the above options and pass in 1 comma seperated string
+    def __init__(self,processors = None, use_gpu=False): #Pass empty string to load all processors or choose from the above options and pass in 1 comma seperated string
         self.processors = processors
         self.model = stanza.Pipeline(lang = 'en', processors = processors, use_gpu = use_gpu)
         self.use_gpu = use_gpu
@@ -41,7 +140,7 @@ class Taggers:
     def spacy_tagger(self,inp):
         doc = self.nlp(inp)
         output = [(token.text, token.pos_, token.dep_) for token in doc]
-        displacy.render(doc, style='dep', jupyter=True, options={'distance': 120})
+        # displacy.render(doc, style='dep', jupyter=True, options={'distance': 120})
         return output
     
     def spacy_tagger_dataset(self,data):
@@ -58,6 +157,59 @@ class Taggers:
             # displacy.render(doc, style='dep', jupyter=True, options={'distance': 120})
         return tags_writer,tags_llm,tags_article
     
+    def calculate_subtree_features(self,text):
+        doc = self.nlp(text)
+
+        def subtree_features(token):
+            lefts = list(token.lefts)
+            rights = list(token.rights)
+
+            # Count the number of left and right subtrees
+            num_left_subtrees = len(lefts)
+            num_right_subtrees = len(rights)
+
+            # Calculate the average left and right subtree height
+            avg_left_subtree_height = sum(t._.depth for t in lefts) / num_left_subtrees if num_left_subtrees > 0 else 0
+            avg_right_subtree_height = sum(t._.depth for t in rights) / num_right_subtrees if num_right_subtrees > 0 else 0
+            return num_left_subtrees, num_right_subtrees, avg_left_subtree_height, avg_right_subtree_height
+        
+        spacy.tokens.Token.set_extension('depth', getter=lambda token: len(list(token.ancestors)), force=True)
+        total_left_subtrees = 0
+        total_right_subtrees = 0
+        total_left_subtree_height = 0
+        total_right_subtree_height = 0
+        for sent in doc.sents:
+            for token in sent:
+                num_left_subtrees, num_right_subtrees, avg_left_subtree_height, avg_right_subtree_height = subtree_features(token)
+                total_left_subtrees += num_left_subtrees
+                total_right_subtrees += num_right_subtrees
+                total_left_subtree_height += avg_left_subtree_height * num_left_subtrees
+                total_right_subtree_height += avg_right_subtree_height * num_right_subtrees
+        avg_left_subtree_height_text = total_left_subtree_height / total_left_subtrees if total_left_subtrees > 0 else 0
+        avg_right_subtree_height_text = total_right_subtree_height / total_right_subtrees if total_right_subtrees > 0 else 0
+
+        return {
+            "avg_left_subtree_height": avg_left_subtree_height_text,
+            "avg_right_subtree_height": avg_right_subtree_height_text,
+            "num_left_subtrees": total_left_subtrees,
+            "num_right_subtrees": total_right_subtrees,
+            }
+
+    def calculate_average_tree_height(self,text_list):
+        total_tree_height = 0
+        num_trees = 0
+        for text in text_list:
+            doc = self.nlp(text)
+            stack = [(sent.root, 1) for sent in doc.sents]
+
+            while stack:
+                node, depth = stack.pop()
+                total_tree_height += depth
+                num_trees += 1
+                stack.extend((child, depth + 1) for child in node.children)
+        avg_tree_height = total_tree_height / max(1, num_trees)
+        return avg_tree_height
+
     def count_tags_percentage(self,tags):
         tag_counts ={}
         total_words = 0
@@ -75,6 +227,18 @@ class Taggers:
 
         return tag_counts
 
+    def phrase_counts(self,text):
+        doc = self.nlp(text)
+        # Extract noun phrases (NP), verb phrases (VP), and adjective phrases (ADJP)
+        np = [chunk for chunk in doc.noun_chunks]
+        vp = [chunk for chunk in doc.noun_chunks if chunk.root.pos_ == 'VERB']
+        adjp = [chunk for chunk in doc.noun_chunks if chunk.root.pos_ == 'ADJ']
+        # Print the results
+        print(f'Number of NP subtrees: {len(np)}, Mean length of NP subtrees: {sum(len(chunk) for chunk in np) / len(np) if np else 0}')
+        print(f'Number of VP subtrees: {len(vp)}, Mean length of VP subtrees: {sum(len(chunk) for chunk in vp) / len(vp) if vp else 0}')
+        print(f'Number of ADJP subtrees: {len(adjp)}, Mean length of ADJP subtrees: {sum(len(chunk) for chunk in adjp) / len(adjp) if adjp else 0}')
+
+
     def count_dependency_arcs(self,data):
         
         left_arcs = []
@@ -87,10 +251,10 @@ class Taggers:
 
         dependency_arcs['number of sentences'] = 0
         for doc in docs:
+            dependency_arcs['number of sentences']+=len(list(doc.sents))
             for token in doc:
                 left_arcs.extend([token.dep_ for _ in token.lefts])
                 right_arcs.extend([token.dep_ for _ in token.rights])
-                dependency_arcs['number of sentences']+=len(list(doc.sents))
                 for child in token.children:
                     if child.i < token.i: 
                         left_arc_lengths.append(abs(token.i - child.i))
@@ -110,6 +274,25 @@ class Taggers:
         dependency_arcs['standard deviation total arcs'] = np.std(left_arc_lengths+right_arc_lengths) 
         
         return dependency_arcs
+
+    def average_word_length_instance(self,text):
+        doc = self.nlp(text)
+        words = [token.text for token in doc if not token.is_punct and not token.is_space]
+        total_length = sum(len(word) for word in words)
+        num_words = len(words)
+        average_word_length = total_length / num_words if num_words > 0 else 0
+        return average_word_length
+
+    def average_word_length_system(self,text_list):
+        total_length = 0
+        num_words = 0
+        for text in text_list:
+            doc = self.nlp(text)
+            words = [token.text for token in doc if not token.is_punct and not token.is_space]
+            total_length += sum(len(word) for word in words)
+            num_words += len(words)
+        average_word_length = total_length / num_words if num_words > 0 else 0
+        return average_word_length
 
     def gender_ratio_single(self,data): # Single string input
         male_count = 0
@@ -148,7 +331,14 @@ class Taggers:
 
         return male_count_writer/female_count_writer, male_count_llm/female_count_llm, male_count_article/female_count_article
    
-    def avg_sentence_length(self,summaries):
+    def avg_sentence_length(self,text):
+        total_tokens = 0
+        sentences= nltk.sent_tokenize(text)
+        total_tokens = sum(len(sentence.split()) for sentence in sentences)
+        average_length= total_tokens / len(sentences)
+        return average_length
+
+    def avg_sentence_length_dataset(self,summaries):
         total_length_writer = 0
         total_length_llm = 0
         total_length_article = 0
@@ -184,3 +374,51 @@ class Emotions:
 
     def sentiments(self,input):
         return self.classifier(input)
+    
+
+
+    # def calculate_subtree_stats(self,sentences):
+    #     overall_sum_left = 0
+    #     overall_num_left = 0
+    #     overall_sum_right = 0
+    #     overall_num_right = 0
+    #     for sentence in sentences:
+    #         doc = self.nlp(sentence)
+    #         left_subtree_heights = [0] * len(doc)
+    #         right_subtree_heights = [0] * len(doc)
+    #         num_left_subtrees = 0
+    #         num_right_subtrees = 0
+    #         for token in doc:
+    #             left_height = 0
+    #             right_height = 0
+    #             # Calculate left subtree height
+    #             for child in token.lefts:
+    #                 left_height = max(left_height, 1 + left_subtree_heights[child.i])
+    #             # Calculate right subtree height
+    #             for child in token.rights:
+    #                 right_height = max(right_height, 1 + right_subtree_heights[child.i])
+    #             left_subtree_heights[token.i] = left_height
+    #             right_subtree_heights[token.i] = right_height
+    #             if left_height > 0:
+    #                 num_left_subtrees += 1
+    #                 overall_sum_left += left_height
+    #             if right_height > 0:
+    #                 num_right_subtrees += 1
+    #                 overall_sum_right += right_height
+
+    #         overall_num_left += num_left_subtrees
+    #         overall_num_right += num_right_subtrees
+
+    #     overall_avg_left = overall_sum_left / overall_num_left if overall_num_left > 0 else 0
+    #     overall_avg_right = overall_sum_right / overall_num_right if overall_num_right > 0 else 0
+    #     max_left_ht = max(left_subtree_heights)
+    #     max_right_ht = max(right_subtree_heights)
+
+    #     return {
+    #         "avg_left_subtree_height": overall_avg_left,
+    #         "avg_right_subtree_height": overall_avg_right,
+    #         "num_left_subtrees": overall_num_left,
+    #         "num_right_subtrees": overall_num_right,
+    #         "Height left subtree": max_left_ht,
+    #         "Height right subtree": max_right_ht,
+    #         }
